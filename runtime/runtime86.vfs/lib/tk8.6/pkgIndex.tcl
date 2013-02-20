8f3953a691e0512e9fad9a1a463a468be7d2f9b0
Goodwin Lawlor <goodwin.lawlor@gmail.com>
2013-02-20 22:04:46 +0400

    Clean up runtime86

--- runtime/runtime86.vfs/lib/tk8.6/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-if {[catch {package present Tcl 8.6b2}]} return
-package ifneeded Tk 8.6b2	[list load [file join $dir .. libtk8.6.so] Tk]
--- runtime/runtime86.vfs/lib/vtkinteraction/pkgIndex.tcl
+++ runtime/runtime86.vfs/lib/vtkinteraction/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded vtkinteraction 5.9 [list source [file join $dir vtkinteraction.tcl]]
+package ifneeded vtkinteraction 5.10 [list source [file join $dir vtkinteraction.tcl]]
